aa1473c84cd88549d56e954a1f6ea445f815c850
Vlvxingze <1207223090@qq.com>
2017-07-08 07:29:24 +0800

    修改API，增加title

--- src/com/mcml/space/eventlimitor/AutoRespawn.java
+++ src/com/mcml/space/eventlimitor/AutoRespawn.java
@@ -26,6 +26,7 @@ public class AutoRespawn implements Listener {
 					try {
 						player.spigot().respawn();
 						if(VLagger.AutoRespawnRespawnTitleenable == true){
+							player.sendTitle(VLagger.AutoRespawnRespawnTitleMainMessage, VLagger.AutoRespawnRespawnTitleMiniMessage);
 						}
 					} catch (Throwable ex) {
 						VLagger.MainThis.getLogger().info("提示:您的服务器不是Spigot服务端？无法启用自动复活功能！");
