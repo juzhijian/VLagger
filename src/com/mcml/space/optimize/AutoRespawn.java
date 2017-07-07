b180a3e51100b2e73714fe027b30c34286e00622
Vlvxingze <1207223090@qq.com>
2017-07-07 14:53:58 +0800

    修复AutoRespawn会自动保存物品的BUG？

--- src/com/mcml/space/optimize/AutoRespawn.java
+++ src/com/mcml/space/optimize/AutoRespawn.java
@@ -4,6 +4,7 @@
  */
 package com.mcml.space.optimize;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,16 +16,20 @@ import com.mcml.space.core.VLagger;
  *
  * @author Administrator
  */
-public class AutoRespawn implements Listener{
-    @EventHandler
-    public void AutoRespawner(PlayerDeathEvent event){
-        Player player = event.getEntity();
-        if(VLagger.AutoRespawnenable == true){
-            try{
-                player.spigot().respawn();
-            }catch(Throwable ex){
-                VLagger.MainThis.getLogger().info("提示:您的服务器不是Spigot服务端？无法启用自动复活功能！");
-            }
-        }
-    }
+public class AutoRespawn implements Listener {
+	@EventHandler
+	public void AutoRespawner(PlayerDeathEvent event) {
+		if (VLagger.AutoRespawnenable == true) {
+			Player player = event.getEntity();
+			Bukkit.getScheduler().runTaskLater(VLagger.MainThis, new Runnable() {
+				public void run() {
+					try {
+						player.spigot().respawn();
+					} catch (Throwable ex) {
+						VLagger.MainThis.getLogger().info("提示:您的服务器不是Spigot服务端？无法启用自动复活功能！");
+					}
+				}
+			}, 3);
+		}
+	}
 }
