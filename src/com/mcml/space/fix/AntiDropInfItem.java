a34389cecd43def73c7c231b8bc0bdc7facb9c4d
Vlvxingze <1207223090@qq.com>
2017-07-08 14:43:40 +0800

    去掉测试信息

--- src/com/mcml/space/fix/AntiDropInfItem.java
+++ src/com/mcml/space/fix/AntiDropInfItem.java
@@ -1,20 +1,16 @@
 package com.mcml.space.fix;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 import com.mcml.space.core.VLagger;
-
-import org.bukkit.event.player.*;
 
 public class AntiDropInfItem
         implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void CheckDropNoBugInfItem(PlayerDropItemEvent e) {
-    	Bukkit.broadcastMessage("丢出监听器被触发");
 		if(VLagger.AntiDropInfItemenable == true){
 			if (e.getPlayer() == null || e.getPlayer().isOnline()==false || e.getPlayer().isValid()==false) {
 				e.setCancelled(true);
